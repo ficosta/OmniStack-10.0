ed3410231582f347ba419f4991579fbb2b8f429f
Felipe <felipeiasi@gmail.com>
2020-01-15 19:09:47 -0300

    Controller de busca por tecnologia e ponto de localização

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,28 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+module.exports = {
+    async index(req, res) {
+        //Buscar todos os devs em um raio de 10km
+        //Filtrar por tech
+        const { latitude, longitude, techs } = req.query
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [parseFloat(longitude), parseFloat(latitude)],
+                        //coordinates: [-46.7275977,-23.6059692],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        })
+
+        return res.json({ devs })
+    }
+}
