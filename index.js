129bd48607bc9f4c5a28c8f48ddd61f6c782fd6e
Felipe <felipeiasi@gmail.com>
2020-01-15 12:15:27 -0300

    instalação dependencias

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.json({message:"Hello guy."})
+})
+
+app.listen(port, () => console.log(`App rodando na porta ${port}`))
